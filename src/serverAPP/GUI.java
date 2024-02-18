d5d9d1ab8f18620967b01438a49cbbddc3af202d
Oscarbre <oscar.chene@live.fr>
2024-02-18 16:09:44 +0100

    commit

--- src/serverAPP/GUI.java
+++ src/serverAPP/GUI.java
@@ -5,8 +5,6 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
-import javafx.scene.Node;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -20,7 +18,6 @@ import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
-import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
 
 //////////////////////////////////////////////////////////              MAIN            ///////////////////////////////////////////////
